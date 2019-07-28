c1001866b37ec3182e4c5e7f49110c9343ff31a3
francescancio <francescancio@Francess-MacBook-Air.local>
2019-07-28 14:32:25 -0500

    Define winning combinations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 /*----- constants -----*/ 
 const COLORS = {
-    '0': 'white',
-    '1': 'purple',
-    '-1': 'orange' 
+    '0': 'null',
+    '1': 'grey',
+    '-1': 'black' 
 };
+
+const winningArr = [];
+    winningArr.push([1, 2, 3]);
+    winningArr.push([4, 5, 6]);
+    winningArr.push([7, 8, 9]);
+    winningArr.push([1, 4, 7]);
+    winningArr.push([2, 5, 8]);
+    winningArr.push([3, 6, 9]);
+    winningArr.push([1, 5, 9]);
+    winningArr.push([3, 5, 7]);
+
 
 /*----- app's state (variables) -----*/ 
 let board, turn, winner;
@@ -17,12 +28,28 @@ let board, turn, winner;
 /*----- functions -----*/
 
 function init() {
-    board = [
-        [0, 0, 0], // column 1 (index 0)
-        [0, 0, 0], // column 2 (index 1)
-        [0, 0, 0], // column 3 (index 2)
- 
+    boardArr = [
+        [1, 0, 0, 0, 0, 0, 0, 0, -1], // board array represeting 9 spaces
     ];
-    turn = 1;
+    turn = 1;  // start with player 1
     winner = null;          // 1, -1, null (no winner), 'T' (tie)
+    render();
 }
+
+function render() {
+    boardArr.forEach(function(i) {
+        let div = document.getElementById(`s${i}`);
+        div.style.backgroundColor = COLORS[cell];
+});
+
+function handleClick(evt) {
+    let idx = parseInt(evt.target.id.replace(`s`, ''));
+
+}
+
+if (winner) {
+1 || -1 === [winArr];
+return 'winMsg';
+
+}
+
